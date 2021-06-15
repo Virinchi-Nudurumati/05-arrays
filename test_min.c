c789f12dd5bc155df9132a5e99327e17c9a1988a
Virinchi-Nudurumati <84717029+Virinchi-Nudurumati@users.noreply.github.com>
2021-06-15 20:30:37 +0530

    Update test_min.c

--- test_min.c
+++ test_min.c
@@ -3,7 +3,19 @@
 
 
 int min(int [], int);
-
+int k;
+int min(int nos[],int count)
+{
+k=nos[0];
+for(int i=0;i<count;i++)
+{
+if(k>nos[i])
+{
+k=nos[i];
+}
+}   
+return k;
+}
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
   assert (min(x, 7) == -3);
